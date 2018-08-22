45bedb049be5858554edaae641c07623fe4b0c1a
upupming <upupming@gmail.com>
2018-08-22 22:24:03 +0800

    Updated: use ecosystem config from pm2 docs

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: 'app',
+    script: './app.js',
+    instances: '1',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    }
+  }]
+};
